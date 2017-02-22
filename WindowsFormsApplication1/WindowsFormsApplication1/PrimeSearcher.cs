2ab212a4787a31270c2e7250effb8f553cf1aa10
FanniLakos1995 <lakosfanni1995@gmail.com>
2017-02-22 20:04:21 +0100

    na ??????

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/PrimeSearcher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    static class PrimeSearcher
+    {
+        static bool prim1(int p)
+        {
+            for (int i = 2; i < p; i++)
+            {
+                if (p % i == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+}
